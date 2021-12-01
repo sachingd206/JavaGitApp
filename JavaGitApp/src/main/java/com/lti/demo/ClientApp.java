93f9e7557202bc4dc54a2c6a95fbd4e1a2edd5fd
sachingd206 <sachingd206@gmail.com>
2021-12-01 14:25:07 +0530

    first commit

---
+++ JavaGitApp/src/main/java/com/lti/demo/ClientApp.java
@@ -0,0 +1,9 @@
+package com.lti.demo;
+
+public class ClientApp {
+	public static void main(String[] args) {
+		Product p1=new Product(101,"Mobile");
+		System.out.println(p1);
+	}
+
+}
---
+++ JavaGitApp/src/main/java/com/lti/demo/Product.java
@@ -0,0 +1,19 @@
+package com.lti.demo;
+
+public class Product {
+	private int prdId;
+		private String prdName;
+		public Product(int prdId, String prdName) {
+			super();
+			this.prdId = prdId;
+			this.prdName = prdName;
+		}
+		@Override
+		public String toString() {
+			return "Product [prdId=" + prdId + ", prdName=" + prdName + "]";
+		}
+		
+		
+
+
+}
