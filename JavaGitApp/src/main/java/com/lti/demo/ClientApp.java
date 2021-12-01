38017f8ddded059d4e757a5bca3d67a9ce2a7daa
sachingd206 <sachingd206@gmail.com>
2021-12-01 15:30:35 +0530

    ThirdCommit

--- JavaGitApp/src/main/java/com/lti/demo/ClientApp.java
+++ JavaGitApp/src/main/java/com/lti/demo/ClientApp.java
@@ -3,6 +3,7 @@ package com.lti.demo;
 public class ClientApp {
 	public static void main(String[] args) {
 		Product p1=new Product(101,"Mobile");
+		Product p2=new Product(102,"car");
 		System.out.println(p1);
 	}
 
